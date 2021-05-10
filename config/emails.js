7a886c022d53f7579ec48e7bca4fd9001e1719b7
Steve Ticona <swattycons@gmail.com>
2021-05-09 23:22:53 -0400

    Confirmacion de Cuenta Gmail

--- config/emails.js
+++ config/emails.js
@@ -4,6 +4,6 @@ require('dotenv').config({ path: 'variables.env' });
 module.exports = {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
-    host:"smtp.mailtrap.io",
-    port: '2525',
+    host:"smtp.gmail.com",
+    port: '465',
 }
